6960edaafe0cbffaf47761daec4776a83a0c3696
Wol1Fram <88076654+Wol1Fram@users.noreply.github.com>
2025-08-16 11:40:27 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     println!("Загадайте число");
 
      match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
+        Ordering::Less => println!("Маленький!"),
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win!"),
     }
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    println!("Секретное число: {secret_number}");
 
     println!(r#"Пожалуйста загадайте свое число"#);
 
