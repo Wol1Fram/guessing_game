ee4b9ac653197891418ab62716e5438ce95bfc42
Wol1Fram <zavenskijvlad@gmail.com>
2025-08-16 09:29:37 +0300

    test
    Implements a number guessing game
    
    Introduces a simple number guessing game where the user attempts to guess a secret number. This involves generating a random secret number and providing feedback to the user based on their guess.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 fn main() {
     println!("Загадайте число");
+
+     match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!(r#"Пожалуйста загадайте свое число"#);
 
